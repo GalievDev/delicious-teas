b7730487affe5bebdb6d9850558ea4304e52f478
galiev <ggvalion@gmail.com>
2023-10-22 16:51:15 +0300

    Initialization

---
+++ src/main/kotlin/dev/galiev/deliciousteas/DeliciousTeas.kt
@@ -0,0 +1,9 @@
+package dev.galiev.deliciousteas
+
+import net.fabricmc.api.ModInitializer
+
+class DeliciousTeas: ModInitializer {
+    override fun onInitialize() {
+        TODO("Not yet implemented")
+    }
+}
